147e69d9ce5e9a017b024e7e044105cdfb58da09
mkpbd <115967667+kamalpassa@users.noreply.github.com>
2025-01-22 13:22:13 +0600

    String interpolation

---
+++ angular19basic/src/app/component/interpolation/interpolation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  imports: [],
+  templateUrl: './interpolation.component.html',
+  styleUrl: './interpolation.component.css'
+})
+export class InterpolationComponent {
+
+    title: string= `String interpolation use curly braces {{}} `;
+}

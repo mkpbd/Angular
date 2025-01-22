0ff6c2ec02f79ba957c48f92c30c98dacc324314
mkpbd <115967667+kamalpassa@users.noreply.github.com>
2025-01-22 18:34:45 +0600

    Event update

--- angular19basic/src/app/component/event-binding/event-binding.component.ts
+++ angular19basic/src/app/component/event-binding/event-binding.component.ts
@@ -21,4 +21,85 @@ export class EventBindingComponent {
   handleOnchange(event: any): void {
     console.log(event.target.value);
   }
+
+  handleMousedown(event: MouseEvent): void {
+    console.log(event.type );
+  }
+
+  handleMouseup(event: MouseEvent): void {
+    console.log(event.type );
+  }
+
+  handleMousemove(event: MouseEvent): void {
+    console.log(event.clientX, event.clientY );
+  }
+
+  handleKeydown(event: KeyboardEvent): void {
+    console.log(event.key );
+  }
+
+  handleKeyup(event: KeyboardEvent): void {
+    console.log(event.key );
+  }
+
+  handleKeypress(event: KeyboardEvent): void {
+    console.log(event.key );
+  }
+  handleWheel(event: WheelEvent): void {
+    console.log(event.deltaY );
+  }
+
+  handleDrag(event: DragEvent): void {
+    console.log(event.clientX, event.clientY );
+  }
+
+  handleDrop(event: DragEvent): void {
+    event.preventDefault();
+    // console.log(event.dataTransfer.getData('text') );
+  }
+  handleCopy(event: ClipboardEvent): void {
+    console.log(event?.clipboardData?.getData('text') );
+
+  }
+  handleCut(event: ClipboardEvent): void {
+    console.log(event?.clipboardData?.getData('text') );
+  }
+  handlePaste(event: ClipboardEvent): void {
+    console.log(event?.clipboardData?.getData('text') );
+  }
+  handleFocus(event: FocusEvent): void {
+    console.log(event.target );
+  }
+  handleBlur(event: FocusEvent): void {
+    console.log(event.target );
+  }
+  handleLoad(event: Event): void {
+    console.log(event.type );
+  }
+  handleError(event: ErrorEvent): void {
+    console.log(event.error );
+  }
+  handleAbort(event: Event): void {
+
+
+    console.log(event.type );
+  }
+
+
+  handleResize(event: UIEvent): void {
+    console.log(event.target );
+    console.log(window.innerWidth, window.innerHeight );
+
+  }
+  handleScroll(event: UIEvent): void {
+    console.log(event.target );
+    console.log(window.scrollX, window.scrollY );
+    }
+    handleSelect(event: Event): void {
+    console.log(event.target );
+    console.log(event.type );
+  }
+
+
+
 }
